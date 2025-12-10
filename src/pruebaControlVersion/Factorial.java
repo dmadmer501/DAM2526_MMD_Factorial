bf56514033969422477557a79e38bd7ffd7e1e8c
dmadmer501 <dmadmer501@g.educaand.es>
2025-12-10 08:56:39 +0100

    B: Agregada función pedir número

---
+++ src/pruebaControlVersion/Factorial.java
@@ -0,0 +1,26 @@
+package pruebaControlVersion;
+
+import java.util.Scanner;
+
+public class Factorial {
+	private static Scanner sc = new Scanner(System.in);
+
+	private static int pedirNumero() {
+	    System.out.print("Introduce un número entero mayor o igual que 0: ");
+	    return sc.nextInt();
+}
+
+	public static void main(String[] args) {
+
+        	int num = pedirNumero();
+        	num = sc.nextInt();
+
+        	if (num < 0) {
+            		System.out.println("ERROR: el número debe ser mayor o igual que 0.");
+        	} else {
+            		System.out.println("Has introducido el número: " + num);
+        	}
+
+	}
+
+}
